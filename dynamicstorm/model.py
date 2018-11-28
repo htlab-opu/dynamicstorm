0f9e3945075f52998c64558d79db22b6940d413c
fiftystorm36@htlab <kenyayanke223@gmail.com>
2018-11-29 03:40:16 +0900

    他のクラスと組み合わせるために処理内容を変更した．

--- dynamicstorm/model.py
+++ dynamicstorm/model.py
@@ -1,58 +1,65 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import tkinter as tk
-
 import glob
 import sys
 
+from . import filter as dyfil
+from . import statistics as dyst
 
-class Expt():
+
+class ExptSet:
     """実験"""
 
     def __init__(self, target_dir):
-        self.location = target_dir
         self.file_list = []
-        self.get_file_list()
-        self.filtered_file_list = []
         self.time_averaged_data = ''
+        self.filter = dyfil.Filter()
+        self.statistics = dyst.Statistics()
+        if not target_dir == '':
+            """
+            インスタンス生成時にディレクトリが
+            指定された場合は指定されたディレクトリ内のファイルを瞬時データとして保持する
+            指定されなかった場合は空とする
+            """
+            self.get_file_list(target_dir)
 
-    def get_file_list(self):
+    def get_file_list(self, location):
         """渡されたディレクトリ内に存在する csv ファイルをリストに格納する"""
-        self.file_list = glob.glob(self.location + '/*.csv')
+        self.file_list = glob.glob(location + '/*.csv')
         if len(self.file_list) == 0:
-            self.file_list = glob.glob(self.location + '/*/*.csv')
+            self.file_list = glob.glob(location + '/*/*.csv')
         if len(self.file_list) == 0:
-            self.file_list = glob.glob(self.location + '/*/.*.csv')
+            self.file_list = glob.glob(location + '/*/.*.csv')
         if len(self.file_list) == 0:
             print('No applicable data in directory.\nExit')
             sys.exit()
 
-    def show_invalid_vector_example(self):
-        """含まれる瞬時データの内100個が持つ誤ベクトル数を表示する"""
-        print('invalid vector example')
-        for i, file in enumerate(self.file_list):
-            status = InstantData(file).get_data('Status')
-            total_invalid_vector = np.sum(status == 1) + np.sum(status == 17)
-            print('-', total_invalid_vector)
-            if i > 100:
-                break
+    def filter(self, filter):
+        self.file_list = self.filter.filter_incorrect_vector(self.file_list, filter)
 
-    def filter_invalid_vector(self):
-        pass
+    def time_averaging(self):
+        self.statistics.time_averaging(self.file_list)
 
+    def join(self, expt_instance_list):
+        """
+        複数の実験データを結合してまとめて取り扱えるようにする
+        時間平均，空間平均済みのデータがある場合は結合後のデータで上書きする
+        """
+        for expt_instance in expt_instance_list:
+            self.file_list.append(expt_instance.filelist)
+        # TODO: 平均済みのデータがあるものとないものを結合する場合の考慮 → 削除でよさげ
 
-class InstantData():
-    "””瞬時データ””"
+class InstantData:
+    """瞬時データ"""
 
     def __init__(self, file):
         self.file = file
-        header_row = self.get_header_row()  # ヘッダ行数を取得
-        self.dataframe = pd.read_csv(self.file, header_row)  # ファイルからデータを読み出し
+        header_row = self.get_header_row(file)  # ヘッダ行数を取得
+        self.dataframe = pd.read_csv(self.file, header=header_row)  # ファイルからデータを読み出し
 
-    def get_header_row(self):
-        """データのヘッダ行数を取得する """
-        file = open(self.file, 'r')
+    @staticmethod
+    def get_header_row(file):
+        """データのヘッダ行数を取得する"""
+        file = open(file, 'r')
         for i, line in enumerate(file):
             if line[0] == 'x':
                 file.close()
@@ -60,4 +67,5 @@ class InstantData():
         file.close()
 
     def get_data(self, label):
+        """指定したラベルのデータ列を取り出す"""
         return self.dataframe[label]
